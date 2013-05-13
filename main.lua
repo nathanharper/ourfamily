f6b1ce0c250a5734ccf50d48766dc71b32c91f80
Nathan Harper <nathan.sam.harper@gmail.com>
2013-05-13 14:10:09 -0400

    refactored a bit so its easier to add arbitrary bouncy sprite objects

--- main.lua
+++ main.lua
@@ -1,9 +1,10 @@
 -- TODO: better separation of data and logic.
 -- local MODE = "static"
 local MODE = "dynamic"
-local nathan,ernie,sansa={},{},{}
+local nathan={}
 local world, mouse_collisions -- the "World" object and a collection of objects colliding with the mouse
-local mouse_joint, clicked_fixture
+local mouse_joint, clicked_ud
+local bounceables = {} -- generic single sprite phsyics objects with sounds
 
 -- "Shallow" table copy; non-recursive
 local function cpy(t)
@@ -14,10 +15,82 @@ local function cpy(t)
   return c
 end
 
+-- Create a generic draggable bouncing rectangle with image and sound assets
+local function create_bounceable(img_path, click_paths, release_paths, x, y, restitution)
+  restitution = restitution or 0.9
+  x = x or 0
+  y = y or 0
+
+  local image
+  if type(img_path) == "string" then
+    image = love.graphics.newImage("assets/" .. img_path)
+  else
+    assert(type(img_path) == "table" and #img_path == 3, "Bounceable image path must be a string or an RGB color table.")
+    image = img_path
+  end
+
+  local click_sounds = {}
+  for _,v in ipairs(click_paths) do
+    assert(type(v) == 'string', "Bounceable sound path must be string")
+    click_sounds[#click_sounds+1] = love.audio.newSource("assets/" .. v, "static")
+  end
+  local release_sounds = {}
+  for _,v in ipairs(release_paths) do
+    assert(type(v) == 'string', "Bounceable sound path must be string")
+    release_sounds[#release_sounds+1] = love.audio.newSource("assets/" .. v, "static")
+  end
+  local body = love.physics.newBody(world, x, y, MODE)
+  local shape = love.physics.newRectangleShape(0, 0, image:getWidth(), image:getHeight())
+  local fixture = love.physics.newFixture(body, shape, 1)
+  fixture:setRestitution(restitution)
+
+  local data_table = {
+    clickable = true,
+    image = image,
+    sounds = {
+      click = click_sounds,
+      release = release_sounds
+    }
+  }
+  fixture:setUserData(data_table)
+  bounceables[#bounceables+1] = fixture -- fixture is all we need to store to retrieve other data
+end
+
+local function load_bounceables()
+  create_bounceable("sansa.png", {"brup.wav"}, {"breeow.wav"}, 400, 400, 1.2)
+  create_bounceable("ernie.png", {"hiss.wav"}, {"rasp.wav"}, 60, 60)
+end
+
+local function draw_bounceables()
+  for _,b in ipairs(bounceables) do
+    local ud = b:getUserData()
+    local body = b:getBody()
+    if type(ud.image) == "table" then
+      -- fill a rectangle according to the RGB triplet
+      love.graphics.setColor(unpack(ud.image))
+      love.graphics.polygon("fill", body:getWorldPoints(b:getShape():getPoints()))
+    else
+      love.graphics.draw(ud.image,
+                         body:getX(),
+                         body:getY(),
+                         body:getAngle(),
+                         1,1,
+                         ud.image:getWidth()/2,
+                         ud.image:getHeight()/2)
+    end
+  end
+end
+
 local function load_nathan()
+  nathan.sounds = {}
+  nathan.sounds.click = {}
+  nathan.sounds.release = {}
+  nathan.sounds.click[1] = love.audio.newSource("assets/hey-chelsea.wav", "static")
+  nathan.sounds.release[1] = love.audio.newSource("assets/i-love-you.wav", "static")
+
   local clickable = { -- user data table for all clickable objects
     clickable = true,
-    table     = nathan
+    sounds = nathan.sounds
   }
 
   nathan.images = {}
@@ -25,12 +98,6 @@ local function load_nathan()
   nathan.images.body = love.graphics.newImage "assets/nbody.png"
   nathan.images.leg = love.graphics.newImage "assets/nleg.png"
   nathan.images.arm = love.graphics.newImage "assets/narm.png"
-
-  nathan.sounds = {}
-  nathan.sounds.click = {}
-  nathan.sounds.release = {}
-  nathan.sounds.click[1] = love.audio.newSource("assets/hey-chelsea.wav", "static")
-  nathan.sounds.release[1] = love.audio.newSource("assets/i-love-you.wav", "static")
 
   nathan.head = {}
     nathan.head.body = love.physics.newBody(world, 650/2, 650/2, MODE)
@@ -82,43 +149,6 @@ local function load_nathan()
   love.physics.newRevoluteJoint(nathan.bod.body, nathan.lleg.body, 650/2+20+xo, 650/2+145+yo, false)
 end
 
-local function load_cats()
-  local sansa_mt = {
-    clickable = true,
-    table     = sansa
-  }
-  local ernie_mt = {
-    clickable = true,
-    table     = ernie
-  }
-
-  sansa.image = love.graphics.newImage "assets/sansa.png"
-  sansa.sounds = {}
-  sansa.sounds.click = {}
-  sansa.sounds.release = {}
-  sansa.sounds.click[1] = love.audio.newSource("assets/brup.wav", "static")
-  sansa.sounds.release[1] = love.audio.newSource("assets/breeow.wav", "static")
-
-  sansa.body = love.physics.newBody(world, 400, 400, MODE)
-  sansa.shape = love.physics.newRectangleShape(0,0,52,100)
-  sansa.fixture = love.physics.newFixture(sansa.body, sansa.shape, 1)
-  sansa.fixture:setRestitution(1)
-  sansa.fixture:setUserData(sansa_mt)
-
-  ernie.image = love.graphics.newImage "assets/ernie.png"
-  ernie.sounds = {}
-  ernie.sounds.click = {}
-  ernie.sounds.release = {}
-  ernie.sounds.click[1] = love.audio.newSource("assets/hiss.wav", "static")
-  ernie.sounds.release[1] = love.audio.newSource("assets/rasp.wav", "static")
-
-  ernie.body = love.physics.newBody(world, 60, 60, MODE)
-  ernie.shape = love.physics.newRectangleShape(0,0,100,150)
-  ernie.fixture = love.physics.newFixture(ernie.body, ernie.shape, 1)
-  ernie.fixture:setRestitution(0.9)
-  ernie.fixture:setUserData(ernie_mt)
-end
-
 local function draw_nathan()
   -- local origin_offset = nathan.head.shape:getRadius()
   love.graphics.draw(nathan.images.head, 
@@ -126,52 +156,45 @@ local function draw_nathan()
                      nathan.head.body:getY(), 
                      nathan.head.body:getAngle(),
                      1, 1, -- scale factor
-                     40,40)
+                     nathan.images.head:getWidth()/2,
+                     nathan.images.head:getHeight()/2)
   love.graphics.draw(nathan.images.body,
                      nathan.bod.body:getX(), 
                      nathan.bod.body:getY(), 
                      nathan.bod.body:getAngle(),
                      1, 1, -- scale factor
-                     30, 70)
+                     nathan.images.body:getWidth()/2,
+                     nathan.images.body:getHeight()/2)
+
+  local half_arm_width = nathan.images.arm:getWidth()/2
+  local half_arm_height = nathan.images.arm:getHeight()/2
   love.graphics.draw(nathan.images.arm,
                      nathan.larm.body:getX(), 
                      nathan.larm.body:getY(), 
                      nathan.larm.body:getAngle(),
                      -1, 1, -- scale factor
-                     35, 10)
+                     half_arm_width, half_arm_height)
   love.graphics.draw(nathan.images.arm,
                      nathan.rarm.body:getX(), 
                      nathan.rarm.body:getY(), 
                      nathan.rarm.body:getAngle(),
                      1, 1, -- scale factor
-                     35, 10)
+                     half_arm_width, half_arm_height)
+
+  local half_leg_width = nathan.images.leg:getWidth()/2
+  local half_leg_height = nathan.images.leg:getHeight()/2
   love.graphics.draw(nathan.images.leg,
                      nathan.lleg.body:getX(), 
                      nathan.lleg.body:getY(), 
                      nathan.lleg.body:getAngle(),
                      1, 1, -- scale factor
-                     10, 35)
+                     half_leg_width, half_leg_height)
   love.graphics.draw(nathan.images.leg,
                      nathan.rleg.body:getX(), 
                      nathan.rleg.body:getY(), 
                      nathan.rleg.body:getAngle(),
                      1, 1, -- scale factor
-                     10, 35)
-end
-
-local function draw_cats()
-  love.graphics.draw(sansa.image,
-                     sansa.body:getX(),
-                     sansa.body:getY(),
-                     sansa.body:getAngle(),
-                     1,1,
-                     26,50)
-  love.graphics.draw(ernie.image,
-                     ernie.body:getX(),
-                     ernie.body:getY(),
-                     ernie.body:getAngle(),
-                     1,1,
-                     50,75)
+                     half_leg_width, half_leg_height)
 end
 
 function love.load()
@@ -182,7 +205,7 @@ function love.load()
   mouse_collisions = {}
 
   load_nathan()
-  load_cats()
+  load_bounceables()
 
   objects = {} -- table to hold all our physical objects
 
@@ -271,13 +294,12 @@ end
 
 function love.mousepressed(x, y, button)
   if button ~= "l" then return end
-  for _,v in pairs(mouse_collisions) do
-    local table = v:getUserData().table
-    clicked_fixture = v
+  for ud,v in pairs(mouse_collisions) do
+    clicked_ud = ud
     mouse_joint = love.physics.newMouseJoint(v:getBody(), x, y)
-    if table.sounds then
-      local randy = math.random(#table.sounds.click)
-      love.audio.play(table.sounds.click[randy])
+    if ud.sounds then
+      local randy = math.random(#ud.sounds.click)
+      love.audio.play(ud.sounds.click[randy])
     end
     return
   end
@@ -286,13 +308,12 @@ end
 function love.mousereleased(x, y, button)
   if button ~= "l" then return end
   if mouse_joint then 
-    local table = clicked_fixture:getUserData().table
-    if table.sounds then
-      local randy = math.random(#table.sounds.release)
-      love.audio.play(table.sounds.release[randy])
+    if clicked_ud.sounds then
+      local randy = math.random(#clicked_ud.sounds.release)
+      love.audio.play(clicked_ud.sounds.release[randy])
     end
     mouse_joint:destroy()
-    mouse_joint = nil; clicked_fixture = nil
+    mouse_joint = nil; clicked_ud = nil
   end
 end
 
@@ -313,7 +334,7 @@ function love.draw()
   love.graphics.polygon("fill", objects.ceil.body:getWorldPoints(objects.ceil.shape:getPoints()))
 
   draw_nathan()
-  draw_cats()
+  draw_bounceables()
 
   love.graphics.setColor(50, 50, 50) -- set the drawing color to grey for the blocks
   love.graphics.polygon("fill", objects.block1.body:getWorldPoints(objects.block1.shape:getPoints()))
